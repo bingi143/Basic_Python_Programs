8773ad736260ac8d96cde1203294f061c99d5b7e
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-16 17:25:34 +0530

    [venkatesh] Add: Python program head and tails percentage

---
+++ 1_head_tails_percentage.py
@@ -0,0 +1,45 @@
+'''
+
+@Author: Venkatesh
+@Date: 2024-08-16 18:00:30
+@Last Modified by: Venkatesh
+@Last Modified time: 2024-08-08 18:00:30
+@Title : Python Program head tails percentage
+
+'''
+
+
+import random
+def m1(number):
+    '''
+          Description: 
+                this function is getting head tails percentage
+          Parameters: 
+                number: taking number as parameter
+          Return : 
+                this function not returning any thing
+    '''
+    if(number<=0):
+        return "Enter Positive number"
+    Head=0
+    Tail=0
+    for i in range(1,number):
+        v=random.random()
+        if(v<0.5):
+            Tail=Tail+1
+        elif(v<1):
+            Head=Head+1
+        Heads_Percentage=Head/number*100
+        Tails_Percentage=Tail/number*100
+        return Heads_Percentage,Tails_Percentage
+    
+
+def main():
+    number=int(input("Enter number:"))
+    Heads_Percentage,Tails_Percentage=m1(number)
+    print("Heads Percentage is :",Heads_Percentage)
+    print("Tails percentage is :",Tails_Percentage)
+
+    
+if __name__=="__main__":
+    main()
